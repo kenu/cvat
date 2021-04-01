e91119355aacf05b30914da3c0bede56641b0e45
mjyeastudio <mjyeastudio>
2021-04-01 23:50:07 +0900

    -MainPage update images

--- cvat-ui/src/pages/main/Main.tsx
+++ cvat-ui/src/pages/main/Main.tsx
@@ -23,6 +23,7 @@ export default function Main() {
                                 </div>
                             </div>
                             <div className='imgContents'>
+                                <img src='https://okdevtv.com/target/style/img/product_mask.png' alt='' />
                             </div>
                         </div>
                     </div>
@@ -45,6 +46,10 @@ export default function Main() {
                                 </div>
                             </div>
                             <div className='imgContents multi'>
+                                <img src='https://okdevtv.com/target/style/img/solution_mask_01.png' alt='' />
+                                <img src='https://okdevtv.com/target/style/img/solution_mask_02.png' alt='' />
+                                <img src='https://okdevtv.com/target/style/img/solution_mask_03.png' alt='' />
+                                <img src='https://okdevtv.com/target/style/img/solution_mask_04.png' alt='' />
                             </div>
                         </div>
                     </div>
